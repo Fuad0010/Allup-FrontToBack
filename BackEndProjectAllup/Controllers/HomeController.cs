6ab9e276a608d0db5f7362678510d13030910149
Fuad0010 <fuadft@code.edu.az>
2022-07-22 20:55:16 +0400

    feat:Added Front html-s to mvc

---
+++ BackEndProjectAllup/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndProjectAllup.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
